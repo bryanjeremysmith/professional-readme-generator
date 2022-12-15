18bcf17621e2f01212ae6b15e9269f0f31b9eac2
BJ Smith <bryanjeremysmith@gmail.com>
2022-12-15 14:23:28 -0800

    requiring inquirer

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 // TODO: Include packages needed for this application
+var inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    console.log("writing to file");
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    console.log("init");
+}
 
 // Function call to initialize app
 init();
